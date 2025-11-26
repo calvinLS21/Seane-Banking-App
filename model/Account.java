f3a8a2c23b1051fb65b2d84fe89adf6d159d957f
calvinLS21 <fcse24-004@thuto.bac.ac.bw>
2025-11-26 09:27:49 +0200

    Update Account.java

--- model/Account.java
+++ model/Account.java
@@ -1,8 +1,12 @@
 package model;
 
 import dao.AccountDAO;
-
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import util.DBConnection;
+import javax.swing.JOptionPane;
+
 
 public abstract class Account {
     private int accountID;
@@ -60,8 +64,39 @@ public abstract class Account {
         return false;
     }
 
-    public List<String[]> transactionHistory() {
-        AccountDAO accountDAO = new AccountDAO();
-        return accountDAO.getTransactionHistory(accountID);
+    public String[][] transactionHistory() {
+        List<String[]> history = new ArrayList<>();
+        String query = "SELECT transactionID, transactionType, amount, transactionDate, toAccountID " +
+                "FROM Transactions WHERE accountID = ? ORDER BY transactionDate DESC";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setInt(1, this.accountID);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String[] row = new String[5];
+                row[0] = String.valueOf(rs.getInt("transactionID"));
+                row[1] = rs.getString("transactionType");
+                row[2] = String.format("BWP %.2f", rs.getDouble("amount"));
+                row[3] = rs.getTimestamp("transactionDate").toString();
+                int toAcc = rs.getInt("toAccountID");
+                row[4] = (rs.wasNull()) ? "-" : String.valueOf(toAcc);
+                history.add(row);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error loading transaction history");
+        }
+
+        // Convert to 2D array
+        String[][] result = new String[history.size()][5];
+        for (int i = 0; i < history.size(); i++) {
+            result[i] = history.get(i);
+        }
+        return result;
     }
+
 }
